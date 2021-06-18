760ccd4c781abb687d4684cf19cf011bbba80fea
Ville Haapavaara <ville.haapavaara@futurice.com>
2021-06-18 13:02:07 +0300

    Remove failing tests
    * GitHub test fails most likely due to changes in github.com itself
    * cookietester doesn't exist anymore

--- test/test-all.js
+++ test/test-all.js
@@ -73,20 +73,6 @@ describe('POST /api/render', () => {
       .expect(400)
   );
 
-  it('render github.com should succeed', () =>
-    request(app)
-      .post('/api/render')
-      .send({ url: 'https://github.com' })
-      .set('content-type', 'application/json')
-      .set('Connection', 'keep-alive')
-      .expect(200)
-      .expect('content-type', 'application/pdf')
-      .then((response) => {
-        const length = Number(response.headers['content-length']);
-        chai.expect(length).to.be.above(1024 * 40);
-      })
-  );
-
   it('html in json body should succeed', () =>
     request(app)
       .post('/api/render')
@@ -144,46 +130,6 @@ describe('POST /api/render', () => {
 
         const length = Number(response.headers['content-length']);
         chai.expect(length).to.be.above(30 * 1024 * 1);
-      })
-  );
-
-  it('cookies should exist on the page', () =>
-    request(app)
-      .post('/api/render')
-      .send({
-        url: 'http://www.html-kit.com/tools/cookietester/',
-        cookies:
-              [{
-                name: 'url-to-pdf-test',
-                value: 'test successful',
-                domain: 'www.html-kit.com',
-              }, {
-                name: 'url-to-pdf-test-2',
-                value: 'test successful 2',
-                domain: 'www.html-kit.com',
-              }],
-      })
-      .set('Connection', 'keep-alive')
-      .set('content-type', 'application/json')
-      .expect(200)
-      .expect('content-type', 'application/pdf')
-      .then((response) => {
-        if (DEBUG) {
-          console.log(response.headers);
-          console.log(response.body);
-          fs.writeFileSync('cookies-pdf.pdf', response.body, { encoding: null });
-        }
-
-        return getPdfTextContent(response.body);
-      })
-      .then((text) => {
-        if (DEBUG) {
-          fs.writeFileSync('./cookies-content.txt', text);
-        }
-
-        chai.expect(text).to.have.string('Number-of-cookies-received-2');
-        chai.expect(text).to.have.string('Cookie-named-url-to-pdf-test');
-        chai.expect(text).to.have.string('Cookie-named-url-to-pdf-test-2');
       })
   );
 
